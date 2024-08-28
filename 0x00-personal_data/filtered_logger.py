1454ca126a77c65bc81c706be23f2ce765cec05c
Mo2men-dev <momensameh5@gmail.com>
2024-08-28 19:38:23 +0000

    0x00-personal_data: task_1/done

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,9 +1,38 @@
 #!/usr/bin/env python3
 """
-Task 0: Regex-ing
+Tasks: Logging and Obfuscation
 """
 import re
+import logging
 from typing import List
+
+
+class RedactingFormatter(logging.Formatter):
+    """
+    Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """
+        inits the class
+        """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Log formatter
+        """
+        record.msg = filter_datum(self.fields,
+                                  self.REDACTION,
+                                  record.getMessage(),
+                                  self.SEPARATOR)
+
+        return super(RedactingFormatter, self).format(record)
 
 
 def filter_datum(fields: List[str], redaction: str,
