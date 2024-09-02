1e48aebc2af27657023e69750855283cba7144a4
Mo2men-dev <momensameh5@gmail.com>
2024-09-02 18:45:49 +0000

    0x01-Basic_authentication: task_1/fixed styling for setup files

--- 0x01-Basic_authentication/models/base.py
+++ 0x01-Basic_authentication/models/base.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """ Base module
 """
+
 from datetime import datetime
 from typing import TypeVar, List, Iterable
 from os import path
 import json
 import uuid
-
 
 TIMESTAMP_FORMAT = "%Y-%m-%dT%H:%M:%S"
 DATA = {}
@@ -126,6 +126,7 @@ class Base():
         """ Search all objects with matching attributes
         """
         s_class = cls.__name__
+
         def _search(obj):
             if len(attributes) == 0:
                 return True
@@ -133,5 +134,5 @@ class Base():
                 if (getattr(obj, k) != v):
                     return False
             return True
-        
+
         return list(filter(_search, DATA[s_class].values()))
